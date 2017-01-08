5170905c88ada4f389cb1f38dbeaf7c5ab0e4036
Dmitry Odintsov <odintsoff@gmail.com>
2017-01-08 05:24:40 +0300

    Basic synchronous asset loading

---
+++ VorpalEngine/sources/utility/asset/Content.cpp
@@ -0,0 +1,17 @@
+#include <vorpal/utility/asset/Content.hpp>
+
+namespace vp
+{
+namespace utility
+{
+namespace asset
+{
+
+Content::Content(std::vector<uint8_t>&& buffer) : m_buffer(buffer)
+{
+
+}
+
+}
+}
+}
