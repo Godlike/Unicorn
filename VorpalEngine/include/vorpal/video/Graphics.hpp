30a3e86bf77c96941b52acfd6c0388f2d2e27124
Nikita Krupitskas <nkrupitskas@protonmail.com>
2017-01-17 02:22:33 +0300

    License update

--- VorpalEngine/include/vorpal/video/Graphics.hpp
+++ VorpalEngine/include/vorpal/video/Graphics.hpp
@@ -1,3 +1,8 @@
+/*
+* Copyright (C) 2017 by 73s studio
+* This code is licensed under the MIT license (MIT) (http://opensource.org/licenses/MIT)
+*/
+
 #ifndef VORPAL_VIDEO_GRAPHICS_HPP
 #define VORPAL_VIDEO_GRAPHICS_HPP
 
--- VorpalEngine/sources/utility/Logger.cpp
+++ VorpalEngine/sources/utility/Logger.cpp
@@ -1,3 +1,8 @@
+/*
+* Copyright (C) 2017 by 73s studio
+* This code is licensed under the MIT license (MIT) (http://opensource.org/licenses/MIT)
+*/
+
 //! Loguru initialization as described in loguru.hpp
 
 #define LOGURU_IMPLEMENTATION 1
