690f93d5b47e5e347e215f43ca554ff7dd4b786c
Nikita Krupitskas <nkrupitskas@protonmail.com>
2017-01-17 13:59:18 +0300

    Grapefruit hello

--- cmake/VorpalEngineConfig.cmake
+++ cmake/VorpalEngineConfig.cmake
@@ -1,3 +1,6 @@
+# Copyright (C) 2017 by Grapefruit Tech
+# This code is licensed under the MIT license (MIT) (http://opensource.org/licenses/MIT)
+
 set(VORPAL_ENGINE_NAME "VorpalEngine" CACHE STRING "Project name for VorpalEngine library")
 
 if (NOT DEFINED VORPAL_ENGINE_ROOT)
