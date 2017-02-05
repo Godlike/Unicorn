f26ce8c9241b8b7063bac113a93418eb5fe401e0
Dmitry Odintsov <odintsoff@gmail.com>
2017-02-06 01:36:05 +0300

    Removed some doxygen warnings

--- VorpalEngine/include/vorpal/VorpalEngine.hpp
+++ VorpalEngine/include/vorpal/VorpalEngine.hpp
@@ -15,10 +15,19 @@ namespace video
 class Graphics;
 }
 
+/** @brief  Engine main class */
 class VORPAL_EXPORT VorpalEngine
 {
 public:
+    /** @brief  Constructs an empty engine */
     VorpalEngine();
+
+    /** @brief  Destructs an engine
+     *
+     *  Also calls Deinit() to release all resources
+     *
+     *  @sa Deinit()
+     */
     ~VorpalEngine();
 
     VorpalEngine(const VorpalEngine& other) = delete;
@@ -27,12 +36,28 @@ public:
     VorpalEngine(VorpalEngine&& other) = delete;
     VorpalEngine& operator=(VorpalEngine&& other) = delete;
 
+    /** @brief  Initializes the engine
+     *
+     *  Initializes storage and @p m_pGraphics
+     *
+     *  @return @c true if initialization was successful, @c false otherwise
+     */
     bool Init();
+
+    /** @brief  Engine's main loop */
     void Run();
+
+    /** @brief  Deinitializes the engine
+     *
+     *  Deinitializes @ref m_pGraphics
+     */
     void Deinit();
 
 private:
+    //! Flag describing if engine was initialized
     bool m_isInitialized;
+
+    //! Pointer to graphics system
     video::Graphics* m_pGraphics;
 };
 }
